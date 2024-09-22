83c2e77a1980060044716714f2a00fd60ec62c23
Johan Bell <belljohan3@gmail.com>
2024-09-22 09:22:04 +0100

    revert

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,32 +1,22 @@
-// eslint.config.js
-import typescript from '@typescript-eslint/eslint-plugin';
-import prettier from 'eslint-plugin-prettier';
-
-export default [
-  {
-    types: 'module',
-    env: {
-      node: true,
-      es2020: true,
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    plugins: {
-      '@typescript-eslint': typescript,
-      prettier,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    rules: {
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
   },
-];
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // Use recommended TypeScript rules
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier
+  ],
+  rules: {
+    'prettier/prettier': 'error', // Show prettier issues as ESLint errors
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
